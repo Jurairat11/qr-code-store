b4c0432d5891fef7569da063800cac1f0352595b
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-07-30 16:57:15 +0700

    edit

--- app/Filament/Pages/UploadPartNo.php
+++ app/Filament/Pages/UploadPartNo.php
@@ -42,7 +42,6 @@ class UploadPartNo extends Page implements HasForms
 
             FileUpload::make('file')
                 ->label('Excel File')
-                ->disk('local')
                 ->directory('imports')
                 ->live()
                 ->required(),
