a99cb936a44545a236ec241b69e236684390f05d
abe27 <taweechai.yue@outlook.co.th>
2025-07-31 11:35:46 +0700

    upload file

--- app/Filament/Resources/PartResource/Pages/ListParts.php
+++ app/Filament/Resources/PartResource/Pages/ListParts.php
@@ -3,8 +3,16 @@
 namespace App\Filament\Resources\PartResource\Pages;
 
 use App\Filament\Resources\PartResource;
+use App\Imports\PartImport;
+use App\Models\Store;
 use Filament\Actions;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\Alignment;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ListParts extends ListRecords
 {
@@ -13,7 +21,40 @@ class ListParts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\Action::make('upload_part')
+                ->label('Upload Part')
+                ->icon('heroicon-o-plus-circle')
+                ->requiresConfirmation()
+                ->modalHeading('Upload Part')
+                ->modalAlignment(Alignment::Center)
+                ->closeModalByClickingAway(false)
+                ->form([
+                    Section::make()
+                        ->schema([
+                            Select::make('store_id')
+                                ->label('Store')
+                                ->options(Store::all()->pluck('store_name', 'id'))
+                                ->required(),
+
+                            FileUpload::make('file')
+                                ->label('Excel File')
+                                ->directory('imports')
+                                ->visibility('public')
+                                ->required(),
+                        ])
+                ])
+                ->action(function (array $data) {
+                    // $data = $this->form->getState();
+                    // dd($data);
+                    $storeId = $data['store_id'];
+                    $filePath = storage_path('app/public/' . $data['file']); // แปลง path ให้ Laravel Excel ใช้ได้
+                    Excel::import(new PartImport($storeId), $filePath);
+
+                    Notification::make()
+                        ->title('Data imported successfully')
+                        ->success()
+                        ->send();
+                }),
         ];
     }
 }
