e18cd3f3acdaad14bc49d84b120298eb79600cb2
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-08-01 12:00:10 +0700

    test upload packing

--- app/Imports/PartImport.php
+++ app/Imports/PartImport.php
@@ -22,7 +22,7 @@ class PartImport implements ToModel, WithHeadingRow
     {
         $exists = Part::where('store_id', $this->storeId)
             ->where('part_no', $row['part_no'])
-            //->where('pac_qty',$row['pac_qty'])
+            ->where('pac_qty',$row['pac_qty'])
             ->exists();
 
         if ($exists) {
@@ -39,7 +39,7 @@ class PartImport implements ToModel, WithHeadingRow
         return new Part([
             'store_id' => $this->storeId,
             'part_no' => $row['part_no'],
-            //'pac_qty' => $row['pac_qty']
+            'pac_qty' => $row['pac_qty']
         ]);
     }
 
